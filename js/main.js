01761b729198ac7bf4aab03e9a2b9a2e9c34cb1a
sandip.p <sandip.p@internal.media.net>
2023-07-05 13:37:16 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $(".mob_search").click(function(){
+      $(".search_bar").toggle();
+    });
+    $(".menu_icon").click(function(){
+        $(".menu").toggle();
+    });
+});
